8c10ff03e466c91f2a7db39aa2ba2670c2f8f75a
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-05-02 19:54:07 +0100

    server set up with handlebars template

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.use('/dist', express.static('dist'));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+	res.render('home');
+});
+
+app.listen(3000, function () {
+	console.log('It works!');
+});
