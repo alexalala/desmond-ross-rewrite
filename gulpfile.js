ef2b73ca9eb99fd2bb53d32201861b660c844266
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-05-06 16:24:01 +0100

    styles and vars set up

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./src/sass/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('watch', function(){ 
+gulp.task('watch', function() { 
 	gulp.watch('./src/sass/*.scss', ['sass']);
 });
